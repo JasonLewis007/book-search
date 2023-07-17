847280a0e0387e5b517d6c15a19df4ede494665a
Jason Lewis <pensfan5050@gmail.com>
2023-07-16 22:28:44 -0400

    2nd commit

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+// Queries
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
